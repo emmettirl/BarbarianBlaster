213f7cd6f5fda34e81781c5ea4b36abe6e5fa48e
Emmett <emmettirl@gmail.com>
2024-06-02 16:57:07 +0100

    Initial Commit

---
+++ scripts/base.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var max_health: int = 5
+
+var current_health: int:
+    set(health_in):
+        current_health = health_in
+        print("health was changed")
+        label_3d.text = str(current_health) + "/" + str(max_health)
+        label_3d.modulate = Color.RED.lerp(Color.WHITE, float(current_health)/float(max_health))
+        if current_health <1:
+            get_tree().call_deferred("reload_current_scene")
+        
+        
+@onready var label_3d: Label3D = $Label3D
+
+func _ready() -> void:
+    current_health = max_health
+
+func take_damage() -> void:
+    current_health -= 1
